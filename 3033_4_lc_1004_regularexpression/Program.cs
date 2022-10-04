656e5f7e78250eb73977d837fb406bdd68f98c29
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-10-04 10:16:14 -0500

    Add project files.

---
+++ 3033_4_lc_1004_regularexpression/Program.cs
@@ -0,0 +1,15 @@
+﻿// Jack Herdejurgen 113436899
+// Oct 4, 2022
+// Regular Expression
+
+using System.Text.RegularExpressions;
+
+string testStr = "123abc456789abc. 123ABC";
+
+// test_string = @"\n123\t";
+
+Regex regex = new Regex(@"ab|cd{2}");
+
+var r = regex.Matches(testStr);
+
+Console.ReadKey();
